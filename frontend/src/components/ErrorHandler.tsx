115141178628867f8a3c97a597379f8da8bb5337
Taps73r <povroznykmisha@gmail.com>
2024-03-23 15:43:07 +0200

    update styles

--- frontend/src/components/ErrorHandler.tsx
+++ frontend/src/components/ErrorHandler.tsx
@@ -12,11 +12,14 @@ interface ErrorHandlerProps {
 
 const ErrorHandlerContainer = styled.div`
     position: absolute;
+    font-size: 22px;
+    font-weight: bold;
+    word-wrap: break-word;
     background-color: white;
-    width: 400px;
-    height: 440px;
+    padding: 20px;
     z-index: 5;
-    top: 20%;
+    max-width: 400px;
+    top: 30%;
     left: 50%;
     transform: translateX(-50%);
     display: flex;
@@ -24,6 +27,24 @@ const ErrorHandlerContainer = styled.div`
     border-radius: 10px;
     overflow: hidden;
     border: 2px solid #4a4e69;
+
+    p {
+        word-wrap: break-word;
+    }
+
+    button {
+        margin-top: 20px;
+        font-size: 20px;
+        padding: 5px 14px;
+        border: 1px solid #b1a7a6;
+        border-radius: 6px;
+        cursor: pointer;
+        transition: 0.5s;
+
+        &:hover {
+            background-color: #eb5e28;
+        }
+    }
 `;
 
 export function ErrorHandler({
